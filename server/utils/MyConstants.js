47059c022eb401184ebc418691a0ee6302f511f7
Hoang Kha <chauhoangkha442@gmail.com>
2023-09-27 18:27:41 +0700

    lab2

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster01.lzjgfli.mongodb.net',
+    DB_USER: 'chauhoangkha',
+    DB_PASS: '123',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: '<jwt_secret>',
+    JWT_EXPIRES: '351636000000', // in milliseconds
+    EMAIL_USER: '<email_user>', // gmail service
+    EMAIL_PASS: '<app_pass>'
+  };
+  module.exports = MyConstants;
